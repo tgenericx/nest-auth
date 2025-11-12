2fd05799ac2e329989d50419f9692e9f4b5a9627
root <root@localhost.localdomain>
2025-11-12 18:24:01 +0000

    add barrel file in src

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './auth.module';
+export * from './auth.service';
+export * from './utils/decorators';
+export * from './dto/login.dto';
+export * from './dto/register.dto';
+export * from './utils/hash.util';
